f32bf21a53e7a976f9500219d4c52043ddc2d250
Ryan Freeman <N00173767@student.iadt.ie>
2021-01-17 22:03:29 +0000

    Added ArticleSkeleton component which displays while the AJAX request to fetch articles has completed

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
 import ArticleItem from './ArticleItem';
+import ArticleSkeleton from './ArticleSkeleton';
 
 class ArticleList extends Component {
     render() {
+        const hasArticles = this.props.articles.length > 0
+
         return (
             <div className="mt-12 pt-8 grid gap-16 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
-                {this.props.articles.slice(0, 5).map(article => {
+                {hasArticles &&
+                this.props.articles.slice(0, this.props.limit).map(article => {
                     return (
-                        <ArticleItem article={article} key={article.id}/>
+                        <ArticleItem
+                            article={article}
+                            key={article.id}/>
                     )
-                })}
+                })
+                }
+                {!hasArticles &&
+                // display 5 article skeletons while waiting for articles to load
+                [...Array(5)].map(() => {
+                    return (
+                        <ArticleSkeleton/>
+                    )
+                })
+                }
             </div>
         )
     }
+}
+
+ArticleList.defaultProps = {
+    limit: -1
 }
 
 export default ArticleList;
---
+++ src/components/ArticleSkeleton.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+class ArticleSkeleton extends Component {
+    render() {
+        return (
+            <div className="animate-pulse">
+                <div>
+                    <div className="inline-block">
+                        <div className="inline-flex items-center px-3 py-0.5 rounded-full bg-gray-200 w-16 h-5">
+                        </div>
+                    </div>
+                </div>
+                <div className="block mt-4">
+                    <div className="w-2/3 bg-gray-200 h-5 rounded-full">
+                    </div>
+                    <div className="mt-3 w-full bg-gray-200 h-5 rounded-full">
+                    </div>
+                    <div className="mt-3 w-full bg-gray-200 h-5 rounded-full">
+                    </div>
+                </div>
+                <div className="mt-6 flex items-start">
+                    <div className="flex-shrink-0">
+                        <div>
+                            <div className="h-10 w-10 bg-gray-200 rounded-full">
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ml-3">
+                        <div className="rounded-full bg-gray-200 w-28 h-5">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ArticleSkeleton;
