a00f6530febd4adf55ba650c320f59ff07f2302d
Ryan Freeman <N00173767@student.iadt.ie>
2021-01-25 22:44:07 +0000

    Updated comment sort order icon

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -52,11 +52,10 @@ class CommentList extends React.Component {
                         </svg>
                         }
                         {sortOrder === 'asc' &&
-                        <svg className="-ml-0.5 mr-2 w-5 h-5" fill="none" stroke="currentColor"
-                             viewBox="0 0 24 24"
+                        <svg className="-ml-0.5 mr-2 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                              xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M19 9l-7 7-7-7"/>
+                                  d="M5 15l7-7 7 7"/>
                         </svg>
                         }
                         <span className="text-sm font-medium text-gray-900">
