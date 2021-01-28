52aaa1ff31291091459e3207109bae9e3326a3f2
Ryan Freeman <N00173767@student.iadt.ie>
2021-01-28 15:47:39 +0000

    Updated CreateArticleModal and DeleteArticleModal style

--- src/components/DeleteArticleModal.js
+++ src/components/DeleteArticleModal.js
@@ -99,7 +99,7 @@ class DeleteArticleModal extends React.Component {
                                         <div
                                             className="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
                                             <button type="button"
-                                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:col-start-2 sm:text-sm"
+                                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:col-start-2 text-sm"
                                                     autoFocus={true}
                                                     onClick={this.onDeleteArticle}>
                                                 {isDeletingArticle &&
@@ -116,7 +116,7 @@ class DeleteArticleModal extends React.Component {
                                                 }
                                             </button>
                                             <button type="button"
-                                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+                                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-1 text-sm"
                                                     onClick={this.props.toggleDeleteArticleModal}>
                                                 Cancel
                                             </button>
